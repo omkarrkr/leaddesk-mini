55d49c5f3f296ba1e6843ca295ca90fd254f431b
Omkar kumar <omkarnwd2005@gmail.com>
2026-07-25 15:25:41 +0530

    Configure CORS for production

--- server/app.js
+++ server/app.js
@@ -13,7 +13,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://leaddesk-mini-liard.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/leads", leadRoutes);
